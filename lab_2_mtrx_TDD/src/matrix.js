e91c83f16fd03bf5c6900b713677b174c91cf80c
Serhii Knysh <knysh.se@gmail.com>
2024-09-18 20:50:36 +0300

    Lab-2: Green  phase of TDD

--- lab_2_mtrx_TDD/src/matrix.js
+++ lab_2_mtrx_TDD/src/matrix.js
@@ -5,7 +5,17 @@ const Mtrx = require('mtrx'); // will
  * For simplicity, the methods of MyMatrix delegate execution to the equivalent methods of the third-party 'mtrx' library.
  */
 class MyMatrix {
-    
+    static zeros(rows, cols) {
+        return Mtrx.zeros(rows, cols);
+    }
+
+    static ones(rows, cols) {
+        return Mtrx.ones(rows, cols);
+    }
+
+    static identity(n) {
+        return Mtrx.eye(n);
+    }    
 }
 
 module.exports = MyMatrix;
